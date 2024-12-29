9aa0ef42a6cf6f7ec3c9a848eea9b7e4b1e507be
ThangPH <hathang2004@gmail.com>
2024-12-29 07:59:01 +0700

    update

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ function backToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
-
 const eventButtons = document.querySelectorAll(".event-button");
 eventButtons.forEach((button) => {
   button.addEventListener("click", toggleEventButton);
@@ -32,54 +31,3 @@ function toggleEventButton() {
     button.textContent = "Quan tâm";
   }
 }
-
-const pagination = document.querySelector(".pagination");
-const pageItems = pagination.querySelectorAll(".page-item");
-const prevButton = pagination.querySelector(".prev");
-const nextButton = pagination.querySelector(".next");
-const urlParams = new URLSearchParams(window.location.search);
-let currentPage = parseInt(urlParams.get("page")) || 1;
-const totalPages = 7;
-function updatePagination() {
-  pageItems.forEach((item) => {
-    const pageNumber = parseInt(item.dataset.page);
-    if (pageNumber) {
-      if (pageNumber === currentPage) {
-        item.classList.add("active");
-      } else {
-        item.classList.remove("active");
-      }
-    }
-  });
-  // Disable nút previous nếu ở trang đầu
-  if (currentPage === 1) {
-    prevButton.setAttribute("aria-disabled", "true");
-    prevButton.classList.add("disabled");
-    prevButton.removeAttribute("href");
-  } else {
-    prevButton.classList.remove("disabled");
-    prevButton.setAttribute("href", `?page=${currentPage - 1}`);
-    prevButton.removeAttribute("aria-disabled");
-  }
-  if (currentPage == totalPages) {
-    nextButton.classList.add("disabled");
-    nextButton.removeAttribute("href");
-  } else {
-    nextButton.classList.remove("disabled");
-    nextButton.setAttribute("href", `?page=${currentPage + 1}`);
-  }
-}
-function handlePageClick(event) {
-  const clickedItem = event.target;
-  const newPage = parseInt(clickedItem.dataset.page);
-  if (!isNaN(newPage)) {
-    currentPage = newPage;
-  } else if (clickedItem.classList.contains("prev")) {
-    currentPage--;
-  } else if (clickedItem.classList.contains("next")) {
-    currentPage++;
-  }
-  updatePagination();
-}
-updatePagination();
-
